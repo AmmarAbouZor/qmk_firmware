c36ba64c1fba60ca9d3fc13b542c8998e63396c6
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-11-21 09:03:30 +0100

    jAdjust Keymapping for backspace
    - Backspace is in the position of Caps-Lock

--- keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
         _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  LT(NAV,KC_ESC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN),  KC_QUOT, LT(NAV,KC_ENT), KC_HOME,
+        _______,  LT(NAV,KC_BSPC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN),  KC_QUOT, LT(NAV,KC_ENT), KC_HOME,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(FN),    LT(NAV,KC_SPC),                   LT(SYMBOL,KC_BSPC),   KC_RALT,  MO(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -131,7 +131,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 // Hold On Other Key Press for Caps-Lock and Enter and the second backspace
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(NAV,KC_ESC):
+        case LT(NAV,KC_BSPC):
         case LT(NAV,KC_ENT):
         case LT(SYMBOL,KC_BSPC):
             // Immediately select the hold action when another key is pressed.
@@ -146,7 +146,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(SYMBOL,KC_SPC):
-        case LT(NAV,KC_ESC):
+        case LT(NAV,KC_BSPC):
         case LT(NAV,KC_ENT):
            return 0;
         default:
