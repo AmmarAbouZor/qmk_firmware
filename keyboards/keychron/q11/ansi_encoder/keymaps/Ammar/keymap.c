13b17686182d97ac23a2a0b96850fe0b39ad1626
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-11-22 20:51:02 +0100

    Q11: Change keymaps
    - Navigation is with the symbols layer now
    - Numpad is now on the right side and the home del stuff are moved to
      the numbers layer
    - The two space bars lead to symbols and navigation layer
    - Time for space has been shorten since it's used with the main
      alternative layer

--- keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
@@ -24,8 +24,8 @@
 enum layers{
     BASE,
     FN,
-    SYMBOL,
-    NAV
+    SYMB_NAV,
+    NUMS
 };
 
 enum custom_keycodes {
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
         _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  LT(NAV,KC_BSPC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN),  KC_QUOT, LT(NAV,KC_ENT), KC_HOME,
+        _______,  LT(NUMS,KC_BSPC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN),  KC_QUOT, LT(NUMS,KC_ENT), KC_HOME,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(FN),    LT(NAV,KC_SPC),                   LT(SYMBOL,KC_BSPC),   KC_RALT,  MO(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(FN),    LT(SYMB_NAV,KC_SPC),                   LT(SYMB_NAV,KC_BSPC),   KC_RALT,  MO(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [FN] = LAYOUT_91_ansi(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
@@ -58,20 +58,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
-    [SYMBOL] = LAYOUT_91_ansi(
+    [SYMB_NAV] = LAYOUT_91_ansi(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,     KC_F12,  _______,            _______,
-        _______,  _______,  KC_TILDE,   KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,    _______,  _______,            _______,
-        _______,  KC_CAPS, KC_EXCLAIM, KC_MINS, KC_PLUS,    KC_EQL,   KC_LCBR, KC_RCBR,  KC_UNDS,  KC_PIPE,  KC_LBRC,  KC_RBRC,  _______,              _______,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  KC_LALT,  KC_LCTL,  KC_LT,  KC_GT,  _______,              _______,  _______,
+        _______,  KC_CAPS,  KC_TILDE,   KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,    _______,  _______,            _______,
+        _______,  KC_LCTL, KC_EXCLAIM, KC_UNDS, KC_MINS,   KC_EQL,  KC_PLUS,   KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_PIPE,  _______,              _______,            _______,
+        _______,  _______,            _______,  _______,  KC_GRV,  KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,    KC_LT,    KC_GT,   KC_BSLS,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
-    [NAV] = LAYOUT_91_ansi(
+    [NUMS] = LAYOUT_91_ansi(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,     KC_F12,  _______,            _______,
-        _______,  _______,  _______,     KC_7,     KC_8,    KC_9,  _______,   _______,  KC_HOME,  KC_END,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  LALT_T(KC_0),  LGUI_T(KC_4),  LSFT_T(KC_5),  LCTL_T(KC_6),  _______,   KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_END,  _______,              _______,            _______,
-        _______,  _______,            _______,     KC_1,    KC_2,     KC_3,   _______,  _______,  KC_PGDN,  KC_PGUP,  _______,   _______,             _______,  _______,
+        _______,  _______,  _______,  _______,  KC_PGUP,  _______,  _______,  _______,     KC_7,    KC_8,      KC_9,  _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  _______,  KC_HOME,  KC_PGDN,   KC_END,  _______,     KC_0,     KC_4,    KC_5,      KC_6,  _______,  _______,              _______,            _______,
+        _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,    KC_1,      KC_2,     KC_3,  _______,             _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
 };
@@ -80,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [SYMBOL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [NAV]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [SYMB_NAV] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [NUMS]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif // ENCODER_MAP_ENABLE
 
@@ -103,6 +103,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case LCTL_T(KC_F):
         case LCTL_T(KC_J):
         case LSFT_T(KC_K):
+        case LT(SYMB_NAV,KC_SPC):
             return TAPPING_TERM - 30;
         case LALT_T(KC_A):
         case LALT_T(KC_SCLN):
@@ -134,9 +135,9 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 // Hold On Other Key Press for Caps-Lock and Enter and the second backspace
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(NAV,KC_BSPC):
-        case LT(NAV,KC_ENT):
-        case LT(SYMBOL,KC_BSPC):
+        case LT(NUMS,KC_BSPC):
+        case LT(NUMS,KC_ENT):
+        case LT(SYMB_NAV,KC_BSPC):
             // Immediately select the hold action when another key is pressed.
             return true;
         default:
@@ -145,12 +146,11 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-// Deactivate quick tap for Caps-Lock and Spacebar and enter
+// Deactivate quick tap for Caps-Lock and enter
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(SYMBOL,KC_SPC):
-        case LT(NAV,KC_BSPC):
-        case LT(NAV,KC_ENT):
+        case LT(NUMS,KC_BSPC):
+        case LT(NUMS,KC_ENT):
            return 0;
         default:
             return QUICK_TAP_TERM;
