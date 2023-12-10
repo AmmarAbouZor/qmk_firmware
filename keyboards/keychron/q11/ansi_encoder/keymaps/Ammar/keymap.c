6a0ccb1bb01b2e192b9e0049e734d4ec743b7caf
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-10 19:00:39 +0100

    Keymap: Return brackets to original & low row...
    - Brackets are returned to their original place.
    - Minus and Equal are returned too.
    - Enter & Backspace & Delete & Tab are in low row now.

--- keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/Ammar/keymap.c
@@ -44,8 +44,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_BSPC,     KC_BSPC,   KC_BSPC,            KC_PGUP,
-        _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,     KC_BSPC,  KC_BSLS,            KC_PGDN,
+        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,     KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,  KC_BSLS,            KC_PGDN,
         _______,  LT(NUMS,KC_ESC), LALT_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, LCTL_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LALT_T(KC_SCLN),  KC_QUOT, LT(NUMS,KC_ENT), KC_HOME,
         _______,  OSM(MOD_LSFT),      KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              OSM(MOD_RSFT),  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(FN),    LT(SYMB_NAV,KC_SPC),                   LT(SYMB_NAV,KC_BSPC),   KC_RALT,  MO(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [SYMB_NAV] = LAYOUT_91_ansi(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,     KC_F12,  _______,            _______,
-        _______,  CW_TOGG,  KC_TILDE,   KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,    _______,  _______,            _______,
+        _______,  CW_TOGG,  KC_TILDE,   KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_LCBR,    KC_RCBR,  _______,            _______,
         _______,  KC_LCTL, KC_EXCLAIM, KC_UNDS, KC_MINS,   KC_EQL,  KC_PLUS,   KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_PIPE,  _______,               KC_APP,            _______,
-        _______,  _______,            _______,   KC_GRV,  KC_LBRC,  KC_RBRC,  CW_TOGG,  KC_LCBR,  KC_RCBR,    KC_LT,    KC_GT,   KC_BSLS,              KC_CAPS,  _______,
+        _______,  _______,            _______,   KC_GRV,  KC_LSFT,  KC_LCTL,   CW_TOGG,  KC_BSPC,   KC_ENT,    KC_LT,    KC_GT,   KC_BSLS,              KC_CAPS,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [NUMS] = LAYOUT_91_ansi(
@@ -175,21 +175,18 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
 // *** Combos ***
 enum combos {
     DF_ESC,
-    KL_ENTER,
-    SD_TAB,
+    KL_TAB,
     ComDot_DEL,
     CV_GERMAN,
 };
 const uint16_t PROGMEM df_combo[] = {LSFT_T(KC_D), LCTL_T(KC_F), COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {LSFT_T(KC_K), LGUI_T(KC_L), COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {LGUI_T(KC_S), LSFT_T(KC_D), COMBO_END};
 const uint16_t PROGMEM comdot_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
 
 combo_t key_combos[] = {
     [DF_ESC]   = COMBO(df_combo, KC_ESC),
-    [KL_ENTER]   = COMBO(kl_combo, KC_ENT),
-    [SD_TAB]   = COMBO(sd_combo, KC_TAB),
+    [KL_TAB]   = COMBO(kl_combo, KC_TAB),
     [ComDot_DEL] = COMBO(comdot_combo, KC_DEL),
     [CV_GERMAN] = COMBO(cv_combo, OSL(FN)),
 };
